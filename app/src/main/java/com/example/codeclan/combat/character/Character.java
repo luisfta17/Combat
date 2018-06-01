50236843d6ade75ec000ee45f6c120de74ea2fdc
Luis Tejero <luisfaridta@gmail.com>
2018-06-01 11:01:36 +0100

    getters and setters done for Character abstract class

--- app/src/main/java/com/example/codeclan/combat/character/Character.java
+++ app/src/main/java/com/example/codeclan/combat/character/Character.java
@@ -1,4 +1,50 @@
 package com.example.codeclan.combat.character;
 
-public class Character {
+public abstract class Character {
+    private String name;
+    private double maxHp, hp, attPower;
+
+    public Character(String name, double maxHp, double hp, double attPower ){
+        this.name = name;
+        this.maxHp = maxHp;
+        this.hp = hp;
+        this.attPower = attPower;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getMaxHp() {
+        return this.maxHp;
+    }
+
+    public double getHp() {
+        return this.hp;
+    }
+
+    public double getAttPower() {
+        return this.attPower;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMaxHp(double maxHp) {
+        this.maxHp = maxHp;
+    }
+
+    public void setHp(double hp) {
+        this.hp = hp;
+    }
+
+    public void setAttPower(double attPower) {
+        this.attPower = attPower;
+    }
+
+
+
+
+
 }
