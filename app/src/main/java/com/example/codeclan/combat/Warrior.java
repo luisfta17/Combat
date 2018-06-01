030474b7b8f0d7f973e99848b530dd76133e4665
Luis Tejero <luisfaridta@gmail.com>
2018-06-01 13:17:28 +0100

    Getters and setters for warrior class added

--- app/src/main/java/com/example/codeclan/combat/Warrior.java
+++ app/src/main/java/com/example/codeclan/combat/Warrior.java
@@ -1,6 +1,7 @@
 package com.example.codeclan.combat;
 
 public class Warrior extends Character {
+
     private double maxAdrenaline, adrenaline;
 
     public Warrior(String name, double maxHp, double hp, double attPower, double maxAdrenaline, double adrenaline ){
@@ -9,10 +10,27 @@ public class Warrior extends Character {
         this.adrenaline = adrenaline;
     }
 
+
+
     public void basicAttack(Character character){
         double extra = getRandomNumber(10);
         character.reciveDamage(this.getAttPower() + extra);
         this.adrenaline += this.getAttPower() + extra;
     }
 
+    public double getMaxAdrenaline() {
+        return this.maxAdrenaline;
+    }
+
+    public void setMaxAdrenaline(double maxAdrenaline) {
+        this.maxAdrenaline = maxAdrenaline;
+    }
+
+    public double getAdrenaline() {
+        return this.adrenaline;
+    }
+    
+    public void setAdrenaline(double adrenaline) {
+        this.adrenaline = adrenaline;
+    }
 }
