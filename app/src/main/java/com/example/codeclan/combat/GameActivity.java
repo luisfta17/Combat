597d4346a1d183346807dd9d079f1c22c644d2ab
Luis Tejero <luisfaridta@gmail.com>
2018-06-02 12:21:42 +0100

    HUD connected to players

--- app/src/main/java/com/example/codeclan/combat/GameActivity.java
+++ app/src/main/java/com/example/codeclan/combat/GameActivity.java
@@ -2,12 +2,32 @@ package com.example.codeclan.combat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+    TextView player_hp;
+    TextView player_adrenaline;
+    TextView android_hp;
+    TextView android_adrenaline;
+    Button basic_att_bttn;
+    Button special_att_bttn;
+    Ninja ninja = new Ninja("Player 1");
+    Knight knight = new Knight("Android");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        player_hp = findViewById(R.id.hp_counter);
+        player_adrenaline = findViewById(R.id.adrenaline_counter);
+        android_adrenaline = findViewById(R.id.android_adrenaline);
+        android_hp = findViewById(R.id.android_hp);
+        basic_att_bttn = findViewById(R.id.basic_att_bttn);
+        special_att_bttn = findViewById(R.id.special_att_bttn);
+        player_hp.setText("HP: " + Double.toString(ninja.getHp()));
+        player_adrenaline.setText("Adrenaline: " + Double.toString(ninja.getAdrenaline()));
+        android_hp.setText("HP: " + Double.toString(knight.getHp()));
+        android_adrenaline.setText("Adrenaline: " + Double.toString(knight.getAdrenaline()));
     }
 }
