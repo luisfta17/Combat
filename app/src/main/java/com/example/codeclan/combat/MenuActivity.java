586337f5644fed3f9b73d5e7bce0a1fa8a700b24
Luis Tejero <luisfaridta@gmail.com>
2018-06-05 21:18:52 +0100

    Ninja added to menu screen, sprites added to drawable

--- app/src/main/java/com/example/codeclan/combat/MenuActivity.java
+++ app/src/main/java/com/example/codeclan/combat/MenuActivity.java
@@ -12,8 +12,11 @@ import android.widget.TextView;
 
 public class MenuActivity extends AppCompatActivity {
     ImageView samuraiImage;
+    ImageView ninjaImage;
     AnimationDrawable samuraiAnimation;
+    AnimationDrawable ninjaAnimation;
     Button play_bttn;
+    Button play_bttn2;
     MediaPlayer mainTheme;
 
     @Override
@@ -21,10 +24,15 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         samuraiImage = findViewById(R.id.samurai_image);
+        ninjaImage = findViewById(R.id.ninja_image);
         samuraiImage.setImageResource(R.drawable.samurai_stands);
+        ninjaImage.setImageResource(R.drawable.ninja_alert);
         play_bttn = findViewById(R.id.play_btn);
+        play_bttn2 = findViewById(R.id.play_bttn2);
         samuraiAnimation = (AnimationDrawable) samuraiImage.getDrawable();
         samuraiAnimation.start();
+        ninjaAnimation = (AnimationDrawable) ninjaImage.getDrawable();
+        ninjaAnimation.start();
         mainTheme = MediaPlayer.create(this, R.raw.maintheme);
         mainTheme.start();
         mainTheme.setLooping(true);
@@ -35,6 +43,15 @@ public class MenuActivity extends AppCompatActivity {
         Intent i=new Intent(MenuActivity.this, GameActivity.class);
         startActivity(i);
         mainTheme.stop();
+        samuraiAnimation.stop();
+        ninjaAnimation.stop();
+    }
 
+    public void onPlayBttn2Clicked(View view){
+        Intent i=new Intent(MenuActivity.this, GameTwoActivity.class);
+        startActivity(i);
+        mainTheme.stop();
+        samuraiAnimation.stop();
+        ninjaAnimation.stop();
     }
 }
