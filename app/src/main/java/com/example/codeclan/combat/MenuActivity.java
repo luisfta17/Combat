8ce5052cf1c2228a788521dd583cd9cb992c7eb5
Luis Tejero <luisfaridta@gmail.com>
2018-06-05 15:34:15 +0100

    Menu activity added

--- app/src/main/java/com/example/codeclan/combat/MenuActivity.java
+++ app/src/main/java/com/example/codeclan/combat/MenuActivity.java
@@ -1,13 +1,40 @@
 package com.example.codeclan.combat;
 
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MenuActivity extends AppCompatActivity {
+    ImageView samuraiImage;
+    AnimationDrawable samuraiAnimation;
+    Button play_bttn;
+    MediaPlayer mainTheme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        samuraiImage = findViewById(R.id.samurai_image);
+        samuraiImage.setImageResource(R.drawable.samurai_stands);
+        play_bttn = findViewById(R.id.play_btn);
+        samuraiAnimation = (AnimationDrawable) samuraiImage.getDrawable();
+        samuraiAnimation.start();
+        mainTheme = MediaPlayer.create(this, R.raw.maintheme);
+        mainTheme.start();
+        mainTheme.setLooping(true);
+    }
+
+
+    public void onPlayBttnClicked(View view){
+        Intent i=new Intent(MenuActivity.this, GameActivity.class);
+        startActivity(i);
+        mainTheme.stop();
+
     }
 }
